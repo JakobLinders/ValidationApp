94c80a4a5c3a7c30e0e8ca46485899ecc8b6ef15
Porcupineridge <linders.jakob@gmail.com>
2019-07-03 13:11:09 +0200

    ignore

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValidationApp
+{
+    class Program
+    {
+        static string inp;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter Zipcode:");
+                inp = Console.ReadLine();
+
+
+                var zz = new ZipCodeService();
+                bool isValid = zz.Validate(inp);
+                if (isValid)
+                {
+                    Console.WriteLine("Zip code is valid. Well done!");
+                }
+                else
+                {
+                    Console.WriteLine("Zip code is invalid. You Suck!");
+                }
+            }
+        }
+
+    }
+}
---
+++ ZipCodeService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidationApp
+{
+    public class ZipCodeService
+    {
+        public bool Validate(string i)
+        {
+
+            if (i == null)
+                return false;
+
+            if (i.Length == 6)
+            {
+                if (i.StartsWith('0'))
+                    return false;
+                else
+                {
+                    for (int a = 0; a < i.Length; a++)
+                    {
+                        if (a == 3)
+                        {
+                            if (i[a] != ' ')
+                                return false;
+                        }
+                        else
+                        {
+                            if (!char.IsDigit(i[a]))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+                return false;
+            return true;
+        }
+        public bool Validate_Regex(string i)
+        {
+
+            return Regex.IsMatch(i, @"[1-9]{1}[0-9]{2} [0-9]{2}$");
+        }
+
+        public int[] SlashZipCode (string zip)
+        {
+            if (Validate(zip))
+            {
+                string[] stringArray = zip.Split(' ');
+
+                int[] array = new int[] { int.Parse(stringArray[0]), int.Parse(stringArray[1]) };
+                return array;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+        }
+    }
+}
